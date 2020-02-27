6a93626b74f3e8e4d4cd20a521e9c35f1999f024
mckib2 <nicholas.bgp@gmail.com>
2020-02-26 19:19:58 -0700

    Remove unneeded linker arg and add -O3 optimization

--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ extensions = [
         'bnb.simplex',
         ['src/csimplex.pyx', 'src/simplex.cpp'],
         include_dirs=['src/'],
-        extra_compile_args=["-std=c++17", "-lstdc++"],
-        extra_link_args=["-std=c++17"],
+        extra_compile_args=["-std=c++17", "-O3"],
+        extra_link_args=[],
         language="c++"),
 ]
 
