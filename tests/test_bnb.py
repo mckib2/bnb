1ae5921bcf8c0d8d2e10074c78f8276c877aca46
mckib2 <nicholas.bgp@gmail.com>
2020-03-12 19:24:14 -0600

    Add more validation problems.

--- tests/test_bnb.py
+++ tests/test_bnb.py
@@ -7,13 +7,19 @@ References
 .. [3] http://web.mit.edu/15.053/www/AMP-Chapter-09.pdf
 .. [4] http://people.brunel.ac.uk/~mastjjb/jeb/or/ip.html#capbud
 .. [5] http://www.math.clemson.edu/~mjs/courses/mthsc.440/integer.pdf
+.. [6] http://web.pdx.edu/~stipakb/download/PA557/ReadingsPA557sec6.pdf
+.. [7] https://www.isical.ac.in/~arijit/courses/autumn2016/ILP-Lecture-1.pdf
+.. [8] https://ncss-wpengine.netdna-ssl.com/wp-content/themes/ncss/pdf/Procedures/NCSS/Mixed_Integer_Programming.pdf
+.. [9] https://www.cs.upc.edu/~erodri/webpage/cps/theory/lp/milp/slides.pdf
 '''
 
 import unittest
 
+import numpy as np
 from bnb import intprog
 
 class TestProblems(unittest.TestCase):
+    '''Validation integer programming problems.'''
 
     def test_prob1(self):
         '''First problem from [1]_.'''
@@ -74,8 +80,7 @@ class TestProblems(unittest.TestCase):
             [.2, .2, .3, .1],
         ]
         b = [3.1, 2.5, .4]
-        bnds = [(0, 1)]*4
-        res = intprog(c, A, b, bounds=bnds)
+        res = intprog(c, A, b, binary=True)
         self.assertEqual(res['x'].tolist(), [0, 0, 1, 1])
         self.assertEqual(res['fun'], .6)
 
@@ -86,10 +91,65 @@ class TestProblems(unittest.TestCase):
             [5, 7, 4, 3],
         ]
         b = [14]
-        bnds = [(0, 1)]*4
-        res = intprog(c, A, b, bounds=bnds)
+        res = intprog(c, A, b, binary=True)
         self.assertEqual(res['x'].tolist(), [0, 1, 1, 1])
         self.assertEqual(res['fun'], 21)
+
+    def test_prob7(self):
+        '''Example problem from page 1 of [6]_.'''
+        c = [12, 40, 15, 20, 10]
+        A = [
+            [.2, .5, .2, .2, .3],
+        ]
+        b = [.8]
+        res = intprog(c, A, b, binary=True)
+        self.assertEqual(res['x'].tolist(), [0, 1, 0, 1, 0])
+        self.assertEqual(res['fun'], 60)
+
+    def test_prob8(self):
+        '''Example problem from page 9 of [7]_.'''
+        c = [8, 11, 6, 4]
+        A = [
+            [5, 7, 4, 3],
+            [7, 0, 2, 1],
+        ]
+        b = [14, 10]
+        res = intprog(c, A, b, binary=True)
+        self.assertEqual(res['x'].tolist(), [0, 1, 1, 1])
+        self.assertEqual(res['fun'], 21)
+
+    def test_prob10(self):
+        '''Example problem from page 2 of [8]_'''
+        # The problem is stated incorrectly for the solution they
+        # give in the text, but if you look at the Dataset table on
+        # page 2 they type it in different than stated above
+        c = [1, 1, 2, -2]
+        A_ub = [
+            [1, 0, 2, 0],
+            [0, 2, 0, -8],
+            [0, -1, 2, -1],
+        ]
+        b_ub = [700, 0, -1]
+        A_eq = [
+            [1, 1, 1, 1],
+        ]
+        b_eq = [10]
+        bnds = [(0, 10)]*4
+        res = intprog(c, A_ub, b_ub, A_eq, b_eq, bounds=bnds)
+        self.assertTrue(np.allclose(res['x'], [3, 4, 2, 1]))
+        self.assertEqual(res['fun'], 9)
+
+    def test_prob11(self):
+        '''Example problem from pg 5 of [9]_.'''
+        c = [1, 1]
+        A = [
+            [2, -2],
+            [-8, 10],
+        ]
+        b = [-1, 13]
+        res = intprog(c, A, b)
+        self.assertEqual(res['x'].tolist(), [1, 2])
+        self.assertEqual(res['fun'], 3)
 
 if __name__ == '__main__':
     unittest.main()
